0cc877148dd32e6814dbb976d57718f27ecf92e7
Haozhe-py <xuhaozhepy@outlook.com>
2026-02-14 19:53:14 +0800

    Update

--- libs/tools/__init__.py
+++ libs/tools/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ['tool_manager', 'file_manager', 'todo_manager', 'outline_manager', 'search']
+__all__ = ['tool_manager', 'file_manager', 'todo_manager', 'outline_manager', 'search', # modules
+           'AIFunction', 'FileManager', 'TODOListManager', 'OutlineManager' # classes & functions
+]
+from .tool_manager import AIFunction
+from .file_manager import FileManager
+from .todo_manager import TODOListManager
+from .outline_manager import OutlineManager
