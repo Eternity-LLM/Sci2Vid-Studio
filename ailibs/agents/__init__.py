8832fe3c3bead2e89956aff67acdcb26e0cc310f
Haozhe-py <xuhaozhepy@outlook.com>
2026-02-18 13:36:10 +0800

    Update __init__.py

--- ailibs/agents/__init__.py
+++ ailibs/agents/__init__.py
@@ -1,3 +1,8 @@
 from .ai_module_class import AIModule
 from .ai_modules import DeepSeekModule, KimiModule, DoubaoModule
 from .mixed_ai_manager import MixedAIManager
+
+__all__ = [
+    'ai_module_class', 'ai_modules', 'mixed_ai_manager', # modules
+    'AIModule', 'DeepSeekModule', 'KimiModule', 'DoubaoModule', 'MixedAIManager'  # classes & functions
+]
